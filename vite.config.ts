39d925e3b28b72705ca267c9ed92c185556593ae
JeanIrad <jado.milton@gmail.com>
2024-05-25 11:57:45 +0200

    [finishes #187354195] setting-up storybook rebasing storybook 	modified:   .eslintrc.json 	modified:   .gitignore 	new file:   .storybook/main.ts 	new file:   .storybook/preview.ts 	modified:   package.json 	new file:   src/stories/SampleComponent.stories.tsx 	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5001
-  }
+    port: 5001,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
 });
